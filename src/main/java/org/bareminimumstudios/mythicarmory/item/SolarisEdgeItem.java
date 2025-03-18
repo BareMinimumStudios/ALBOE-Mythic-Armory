68fe7f17916c15cc1c90855843c979cea63d8e1f
RosemaryThyme <jamieclarkson08@gmail.com>
2025-03-18 20:57:04 +0000

    Made the empowered SolarisEdge fully depower when being brought out of the sun for too long

--- src/main/java/org/bareminimumstudios/mythicarmory/item/SolarisEdgeItem.java
+++ src/main/java/org/bareminimumstudios/mythicarmory/item/SolarisEdgeItem.java
@@ -2,8 +2,6 @@ package org.bareminimumstudios.mythicarmory.item;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.bareminimumstudios.mythicarmory.util.HelperMethods;
 
@@ -11,7 +9,7 @@ import java.util.Objects;
 
 public class SolarisEdgeItem extends DivineSwordItem {
     public static String formNbt = HelperMethods.identifierOf("form").toString();
-    public int ticksInSun = 0;
+    public static int ticksInSun = 0; // I'm gonna change this out for nbt stuffs later
 
     public SolarisEdgeItem(int attackDamage, float attackSpeed, Settings settings) {
         super(attackDamage, attackSpeed, settings);
@@ -35,30 +33,33 @@ public class SolarisEdgeItem extends DivineSwordItem {
         // To add particle effect
     }
 
+    public static void depower(World world, ItemStack stack) {
+        setForm(stack, world.isDay() ? Form.DAY : Form.NIGHT);
+        ticksInSun = 0;
+
+        // To add particle effect
+    }
+
     @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         super.inventoryTick(stack, world, entity, slot, selected);
 
         // Swap form
-        if(world.isNight()) {
-            setForm(stack, Form.NIGHT);
-        } else {
-            if(isForm(stack, Form.NIGHT)) {
-                setForm(stack, Form.DAY);
-            }
+        if(!isForm(stack, Form.EMPOWERED)) {
+            setForm(stack, world.isDay() ? Form.DAY : Form.NIGHT);
         }
 
         // Tick empowerment
         if(selected && world.isDay() && world.isSkyVisible(entity.getBlockPos())) {
-            this.ticksInSun = Math.min(this.ticksInSun+1, 400);
+            ticksInSun = Math.min(ticksInSun+1, 400);
 
-            if(isForm(stack, Form.DAY) && this.ticksInSun > 300) {
+            if(isForm(stack, Form.DAY) && ticksInSun > 300) {
                 empower(world, stack);
             }
         } else {
-            this.ticksInSun = Math.max(this.ticksInSun-1, 0);
-            if(this.ticksInSun <= 300) {
-                setForm(stack, world.isDay() ? Form.DAY : Form.NIGHT); // De-power
+            ticksInSun = Math.max(ticksInSun-1, 0);
+            if(ticksInSun <= 300) {
+                depower(world, stack);
             }
         }
     }
